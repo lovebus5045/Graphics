cf238eb30b987434a63e5e4d9d2da08311c599f0
lovebus5045 <qkrwltn10@naver.com>
2019-03-27 10:31:49 +0900

    V0.13Initial

--- GraphicsEditor-v0.12/src/drawingPanel/DrawingPanel.java
+++ GraphicsEditor-v0.12/src/drawingPanel/DrawingPanel.java
@@ -37,52 +37,76 @@ public class DrawingPanel extends JPanel {
 	}
 
 	// 내일 모래까지 도형을 구상해와라
-	private void drawShape(int x, int y) {
-
+	private void drawShape() {
+		// 그냥 그린다
 		Graphics graphics = this.getGraphics();
 		graphics.setXORMode(getBackground());
-		this.CurrentTool.setOrign(x, y);
 		this.CurrentTool.draw(graphics);
 
 	}
 
-	private void moveShape(int x, int y) {
-		Graphics graphics = this.getGraphics();
-		graphics.setXORMode(getBackground());
+	private void initDrawing(int x, int y) {
+		this.CurrentTool.setOrign(x, y);
+		this.drawShape();
 
-		this.CurrentTool.draw(graphics);
+	}
+
+	private void KeepDrawing(int x, int y) {
+		this.drawShape();
 		this.CurrentTool.setPoint(x, y);
-		this.CurrentTool.draw(graphics);
+		this.drawShape();
 
-		// 점선이 됐다가 실선이 되게 해라
+	}
 
+	private void ContinueDrawing(int x, int y) {
+		// k-1에서 k번째의 점을 찍는 아이
+		// 완전히 polygon을 위한 것을 만들어도 되는데 그렇게 할 경우 actionCommand에서 if문이 들어가게 된다.
+		this.CurrentTool.addPoint(x, y);
+		// setPoint는 마지막점
+		// init --> keepdrawing -->finish --> continue -->keep -->continue
+		// -->finish(마무리) Polygon은 마지막 점이 원점이다.
+		// 중간점은 polygon Line으로 그려도 된다.
+	}
+
+	private void FinishDrawing(int x, int y) {
+
+		this.drawShape();
+		this.CurrentTool.setPoint(x, y);
+		this.drawShape();
 	}
 
 	private class MouseHandler implements MouseListener, MouseMotionListener {
 
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			// click move click move하다가 끝내고 싶을때 끝내야한다.
-			// 여기서 더블클릭을 확인해야한다.
+			if (e.getClickCount() == 1) {
+				initDrawing(e.getX(),e.getY());
+				//flag만들어서 boolean true false사용하기
+				//userinterface에 pattern이 무엇인지 물어본다
+				
+			}else if(e.getClickCount() == 2) {
+				
+			}
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
 			// 다음시간에는 polygon을 그려와라
 
-			drawShape(e.getX(), e.getY());
+			initDrawing(e.getX(), e.getY());
 
 		}
 
 		@Override
 		public void mouseReleased(MouseEvent e) {
 
+			FinishDrawing(e.getX(), e.getY());
 		}
 
 		@Override
 		public void mouseDragged(MouseEvent e) {// 여기서 지운다
 
-			moveShape(e.getX(), e.getY());
+			KeepDrawing(e.getX(), e.getY());// 마우스 움직일때
 
 		}
 
@@ -100,5 +124,4 @@ public class DrawingPanel extends JPanel {
 		}
 	}
 
-	
 }
